a76b86b043a6d89d2d0b718dfb2786bc7451f602
Denis Park <des1991@gmail.com>
2019-03-24 18:45:46 +0300

    Fix tasks

--- Lesson1/task1.rb
+++ Lesson1/task1.rb
@@ -2,10 +2,10 @@ print "Как Вас зовут? "
 name = gets.chomp.capitalize
 
 print "Укажите Ваш рост: "
-tall = gets.chomp.to_i - 100
+tall = gets.to_i - 100
 
 if tall < 0
-	puts "#{name}, Ваш вес уже оптимальный"
+  puts "#{name}, Ваш вес уже оптимальный"
 else
-	puts "#{name}, Ваш идеальный вес #{tall}"
+  puts "#{name}, Ваш идеальный вес #{tall}"
 end
--- Lesson1/task3.rb
+++ Lesson1/task3.rb
@@ -7,25 +7,13 @@ b = gets.chomp.to_f
 print "Сторона треугольника C: "
 c = gets.chomp.to_f
 
-if a > b && a > c
-	g = a
-	c1 = b
-	c2 = c 
-elsif b > a && b > c
-	g = b
-	c1 = a
-	c2 = c
+triangle = [a, b, c].sort
+    
+if triangle[2]**2 == triangle[0]**2 + triangle[1]**2
+  puts "Треугольник прямоугольный."
+  puts "Треугольник равнобедренный." if triangle[0] == triangle[1]
+elsif triangle[0] == triangle[1] && triangle[0] == triangle[2]
+  puts "Треугольник равнобедренный и равносторонний."
 else
-	g = c
-	c1 = a
-	c2 = b
-end
-		
-if g**2 == c1**2 + c2**2
-	puts "Треугольник прямоугольный."
-	puts "Треугольник равнобедренный." if c1 == c2
-elsif c1 == c2 && c1 == g
-	puts "Треугольник равнобедренный и равносторонний."
-else
-	puts "Треугольник не прямоугольный."
+  puts "Треугольник не прямоугольный."
 end
